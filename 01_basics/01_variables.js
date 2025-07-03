66ebf7a4bc937d6e81cb58a91cff765ca8185330
AkshitaTiwari1996 <akshitatiwari2006@gmail.com>
2025-07-03 16:57:36 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 114445
+let accountEmail = "akshute@gmail.com"
+var accountPassword = "235533"
+accountCity = "Jaipur"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword , accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello")  we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log(typeof age);
+console.log(typeof null); //null is an object
