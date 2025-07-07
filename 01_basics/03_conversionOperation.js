dd193f7bce61955db84728bb5d1bb8f38b367c60
AkshitaTiwari1996 <akshitatiwari2006@gmail.com>
2025-07-07 11:12:37 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+/*let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); //conversion of string in number
+console.log(valueInNumber);
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber)) 
+*/
+//******************************OPERATIONS*********************
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = " aksh"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" +2);
+console.log(1+ "2");
+console.log("1" + 2 +2);
+console.log(1+ 2 + "2");
+console.log( (3+4) *5 % 3);
+
+
+
+
+
+
+
+
